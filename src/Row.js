e0bc57af56e99105d03624204b871cfc9c2049db
EenMacFullstack <Iain.mcdulling@gmail.com>
2021-12-08 08:30:45 +0000

    Feature that allows a picture to popup below the poster when the movie poster is clicked

--- src/Row.js
+++ src/Row.js
@@ -1,14 +1,14 @@
 import React, {useState, useEffect} from 'react';
 import axios from "./axios";
-import "./Css/Row.css"
-import YouTube from "react-youtube";
-import movieTrailer from "movie-trailer";
+import "./Css/Row.css";
 
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 function Row({ title, fetchURL, isLargeRow }) {
     const [movies, setMovies] = useState([]);
-    // const [trailerUrl, setTrailerUrl] = ("");
+    const [image, setImage] = useState(undefined);
+    
+
     
     useEffect(() => {
         async function fetchData(){
@@ -19,44 +19,26 @@ function Row({ title, fetchURL, isLargeRow }) {
         fetchData();
     }, [fetchURL]);
 
-    const opts = {
-        heights: "390",
-        width: "100%",
-        playerVars: {
-            autoplay: 1,
-        }
-    };
+     
 
-    // const handleClick = (movie) => {
-    //     if (trailerUrl){
-    //         setTrailerUrl("");
-    //     } else {
-    //         movieTrailer(movie?.name || "")
-    //             .then((url) => {
-    //             const urlParams = new URLSearchParams(new URL(url).search);
-    //             setTrailerUrl(urlParams.get("v"));
-    //         })
-    //         .catch((error) => console.log(error));
-    //     }
-    // };
+    console.log(1 === "1");
 
-    // console.table(movies);
+
 
     return (
         <div className = "row">
             <h2 className="title">{title}</h2>
-            
             <div className = "row-posters">
-                {movies.map(movie =>  (
-                  <img 
-                    className ={`row-poster ${isLargeRow && "row_poster"}`} 
+                {movies.map((movie) =>  (
+                    <img 
+                    className={`row-poster ${isLargeRow && "row-posterLarge"}`} 
                     key={movie.id}
-                    // onClick = {() => handleClick(movie)}
+                    onClick = {()=> handleClick(movie)}
                     src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`}
                     alt={movie.name}/>
-                ))}
+                ))};
             </div>
-            <YouTube videoId="RzyCtVI863w&ab_channel=RugbyPass" opts={opts}/>
+           {image && <img src={image} className="rendered-image" />}
         </div>
     )
 }
